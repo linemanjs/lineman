1110129c8eb661b4f8b037429fccdd4555635940
Justin Searls <searls@gmail.com>
2012-10-19 14:50:25 -0400

    fixes spec-ci b/c testem apparently exits failure with exit code zero for some odd reason

--- tasks/spec-ci.js
+++ tasks/spec-ci.js
@@ -8,18 +8,31 @@
 module.exports = function(grunt) {
   var _ = grunt.utils._,
       path = require('path'),
-      fork = require('child_process').fork;
+      fork = require('child_process').spawn;
 
   grunt.registerTask('spec-ci', 'run specs in ci mode', function(target){
     var done = this.async();
-    var args = ["ci", "-f", path.resolve(process.cwd()+"/config/spec.json")];
-    child = fork(process.cwd()+"/node_modules/testem/testem.js", args)
+        args = ["ci", "-f", path.resolve(process.cwd()+"/config/spec.json")],
+        child = fork(process.cwd()+"/node_modules/testem/testem.js", args),
+        output = "";
+
+    child.stdout.on('data', function(chunk){
+      process.stdout.write(chunk);
+      output += chunk;
+    });
     child.on('exit', function(code, signal){
-      if(code !== 0) {
-        grunt.warn("Spec execution failed with exit code "+code);
+      if(code !== 0 || testsFailed(output)) {
+        grunt.warn("Spec execution appears to have failed.");
+        done(false);
+      } else {
+        done();
       }
-      done();
     });
   });
+};
 
+var testsFailed = function(output) {
+  var lines = output.split("\n"),
+      summaryLine = lines[lines.length - 2];
+  return summaryLine !== "# ok";
 };
