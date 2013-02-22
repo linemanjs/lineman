001cb694b187bd119d6a971b735a2cb81dc0ffea
Andrew Vida <andy@testdouble.com>
2013-02-22 13:46:03 -0500

    minor comment refactor of parent objects that were changed in #35

--- archetype/config/application.js
+++ archetype/config/application.js
@@ -2,7 +2,7 @@
  *  all of the configuration needed by the projects'
  *  depended-on grunt tasks.
  *
- * You can find the parent object in: node_modules/lineman/config/application.js
+ * You can find the parent object in: node_modules/lineman/config/application.coffee
  */
 
 module.exports = require('lineman').config.extend('application', {
