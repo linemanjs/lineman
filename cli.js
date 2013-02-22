28281186bafa4f838a8718b11d58335599da108e
Justin Searls <searls@gmail.com>
2013-02-22 15:13:23 -0500

    lineman new foo --skip-install
    Skips install.
    
    Also a bunch of little cli improvements around
    option pass-thru to grunt.

--- cli.js
+++ cli.js
@@ -1,68 +1,57 @@
 #!/usr/bin/env node
 require('coffee-script')
-var program  = require('commander'),
-    grunt    = require('grunt'),
-    files    = require('./lib/file-utils'),
-    npm      = require('./lib/npm-utils'),
-    cli      = require('grunt/lib/grunt/cli'),
-    _        = grunt.util._;
+var program = require('commander'),
+    grunt   = require('grunt'),
+    files   = require('./lib/file-utils'),
+    npm     = require('./lib/npm-utils'),
+    cli     = require('grunt/lib/grunt/cli'),
+    _       = grunt.util._;
 
-program.version(require('./package').version);
-
-var sprinkleGruntOptions = function(program) {
-  return _(cli.optlist).inject(function(memo, v, k){
-    return memo.option("--"+k);
-  }, program);
-};
 
 program
-    .command('new')
-    .description(' - generates a new lineman project in the specified folder : lineman new my-project')
-    .action(function(project_name) {
-      var src  = __dirname + "/archetype/",
-          dest = process.cwd() + "/" + project_name;
-          console.log(" - Assembling your new project directory in '" + dest + "'...");
-          files.copyDir(src, dest);
-          files.overwritePackageJson(dest + "/package.json", project_name);
-          files.copy(dest + "/.npmignore", dest + "/.gitignore")
-          npm.installFrom(dest, function(error) {
-            if(error) {
-              console.info("Oh no! Please consider opening a new issue with your log output at:\n"+
-                           "\nhttps://github.com/testdouble/lineman/issues \n\n"+
-                           "We're terribly sorry for the inconvenience!");
-            } else {
-              console.info(' - Created a new project in "'+project_name+'/" with Lineman. Yay!\n'+
-                          '\n'+
-                          'Getting started:\n'+
-                          '  1. `cd '+project_name+'` into your new project directory\n'+
-                          '  2. Start working on your project!\n'+
-                          '    * `lineman run` starts a web server at http://localhost:8000\n'+
-                          '    * `lineman build` bundles a distribution in the "dist" directory\n'+
-                          '    * `lineman clean` empties the "dist" and "generated" directories\n'+
-                          '    * `lineman spec` runs specs from the "specs" directory using testem'+
-                          '\n'+
-                          'For more info, check out http://github.com/testdouble/lineman');
-            }
-          });
-    });
+  .version(require('./package').version)
+  .option("--skip-install", "Skip the `npm install` step when creating a new project")
+_(cli.optlist).each(function(option, name){
+  if(!_(["help", "version"]).include(name)) {
+    program.option((option.short ? "-"+option.short+", " : "")+"--"+name, option.info);
+  }
+});
 
-sprinkleGruntOptions(
-  program
-    .command('run')
-    .description(' - runs the development server from /generated and watches files for updates'))
-    .action(function(context) {
-      cli.tasks = ["common", "dev"];
-      grunt.cli();
-    });
+program
+  .command('new')
+  .description(' - generates a new lineman project in the specified folder : lineman new my-project')
+  .action(function(projectName) {
+    var src  = __dirname + "/archetype/",
+        dest = process.cwd() + "/" + projectName;
+    console.log(" - Assembling your new project directory in '" + dest + "'...");
+    files.copyDir(src, dest);
+    files.overwritePackageJson(dest + "/package.json", projectName);
+    files.copy(dest + "/.npmignore", dest + "/.gitignore")
+    if(program.skipInstall) {
+      console.log(" - Skipping `npm install`. You may need to run this yourself while inside the new `"+projectName+"` directory later.");
+      printWelcome(projectName);
+    } else {
+      npm.installFrom(dest, function(error) {
+        error ? printError() : printWelcome(projectName);
+      });
+    }
+});
 
-sprinkleGruntOptions(
-  program
-    .command('build')
-    .description(' - compiles all assets into a production ready form in the /dist folder'))
-    .action(function() {
-      cli.tasks = ["common", "dist"];
-      grunt.cli();
-    });
+program
+  .command('run')
+  .description(' - runs the development server from /generated and watches files for updates')
+  .action(function(context) {
+    cli.tasks = ["common", "dev"];
+    grunt.cli();
+  });
+
+program
+  .command('build')
+  .description(' - compiles all assets into a production ready form in the /dist folder')
+  .action(function() {
+    cli.tasks = ["common", "dist"];
+    grunt.cli();
+  });
 
 program
     .command('spec')
@@ -95,4 +84,25 @@ program
       program.help()
     });
 
-program.parse(process.argv);
+var printError = function(){
+  console.info("Oh no! Please consider opening a new issue with your log output at:\n"+
+               "\nhttps://github.com/testdouble/lineman/issues \n\n"+
+               "We're terribly sorry for the inconvenience!");
+};
+
+var printWelcome = function(projectName){
+  console.info(' - Created a new project in "'+projectName+'/" with Lineman. Yay!\n'+
+              '\n'+
+              'Getting started:\n'+
+              '  1. `cd '+projectName+'` into your new project directory\n'+
+              '  2. Start working on your project!\n'+
+              '    * `lineman run` starts a web server at http://localhost:8000\n'+
+              '    * `lineman build` bundles a distribution in the "dist" directory\n'+
+              '    * `lineman clean` empties the "dist" and "generated" directories\n'+
+              '    * `lineman spec` runs specs from the "specs" directory using testem'+
+              '\n'+
+              'For more info, check out http://github.com/testdouble/lineman');
+
+};
+
+program.parse(process.argv)
