af23032c3344d991a511aca175a3c6b501c8d8df
Justin Searls <searls@gmail.com>
2013-02-24 17:06:36 -0500

    Reorganized how shelling was happening so that it'll fail faster if any of the main lineman commands exit non-zero

--- test/spec/support/lineman_actions.rb
+++ test/spec/support/lineman_actions.rb
@@ -3,25 +3,47 @@ module LinemanActions
   BIN= File.join(ROOT, "cli.js")
 
   def lineman_new
-    `
-    mkdir -p tmp
-    cd tmp
-    rm -rf pants
-    #{BIN} new pants --skip-install
-    cd pants
-    mkdir -p node_modules
-    ln -s #{ROOT} node_modules/lineman
-    `
+    sh <<-BASH
+      mkdir -p tmp
+      cd tmp
+      rm -rf pants
+      #{BIN} new pants --skip-install
+    BASH
+    lineman_link
+  end
+
+  def lineman_link
+    sh <<-BASH
+      cd tmp/pants
+      mkdir -p node_modules
+      ln -s #{ROOT} node_modules/lineman
+    BASH
   end
 
   def lineman_build
-    `
-    cd tmp/pants
-    #{BIN} build --stack
-    `
+    sh <<-BASH
+      cd tmp/pants
+      #{BIN} build --stack
+    BASH
   end
 
   def lineman_tear_down
-    `rm -rf tmp`
+    sh 'rm -rf tmp'
+  end
+
+private
+
+  def sh(command)
+    `#{command}`.tap do |output|
+      unless $?.success?
+        raise <<-ERR
+          Command  failed! You ran:
+          #{command}
+
+          STDOUT:
+          #{output}
+        ERR
+      end
+    end
   end
 end
