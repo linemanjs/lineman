72922c51b1b52b3b9aaca57167710a9b0cf1cddf
Justin Searls <searls@gmail.com>
2013-05-10 01:17:04 -0400

    > require('lineman').version

--- lineman.js
+++ lineman.js
@@ -1,11 +1,14 @@
+require('coffee-script');
 var extend = require("whet.extend");
+
 module.exports = {
   config: {
-    application: require("./config/application"),
-    files: require("./config/files"),
-    grunt: require("./config/grunt"),
+    application: require(__dirname+"/config/application"),
+    files: require(__dirname+"/config/files"),
+    grunt: require(__dirname+"/config/grunt"),
     extend: function(key, stuff) {
       return extend(true, {}, module.exports.config[key], stuff);
     }
-  }
+  },
+  version: require(__dirname+"/package").version
 };
