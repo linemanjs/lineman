1ef7eda0725d57922d288f1b2dbc5d050981ae8c
Justin Searls <searls@gmail.com>
2013-06-26 10:21:23 -0600

    Spells Lineman more correctly.

--- archetype/config/application.js
+++ archetype/config/application.js
@@ -2,7 +2,7 @@
  *  all of the configuration needed by the projects'
  *  depended-on grunt tasks.
  *
- * You can familiarize yourself with all of Linemna's defaults by checking out the parent file:
+ * You can familiarize yourself with all of Lineman's defaults by checking out the parent file:
  * https://github.com/testdouble/lineman/blob/master/config/application.coffee
  */
 
