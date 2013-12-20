94e85e2d7aaa393473e0d65979cfc49ce132bd25
Justin Searls <searls@gmail.com>
2013-12-20 10:33:38 -0500

    oops.

---
+++ test/script/prepare-selenium.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Expected to be run from repo root.
+
+mkdir -p test/bin
+cd test/bin
+
+SELENIUM_VERSION=${SELENIUM_VERSION-2.38.0}
+
+export SELENIUM_SERVER_JAR="$(pwd)/selenium-server-standalone-$SELENIUM_VERSION.jar"
+
+if [ ! -f $SELENIUM_SERVER_JAR ]; then
+  wget http://selenium.googlecode.com/files/selenium-server-standalone-$SELENIUM_VERSION.jar
+fi
+
+# Drop stuff where webdriver-sync wants to see it.
+mkdir ~/.webdriver-sync
+ln -s $SELENIUM_SERVER_JAR "$HOME/.webdriver-sync"
+touch ~/.webdriver-sync/chromedriver
